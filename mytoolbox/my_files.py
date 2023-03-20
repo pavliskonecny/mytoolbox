bba513f0ae36a248e5acfb792bdc090b7bf88249
Pavel Konecny <pavel.konecny@hella.com>
2023-03-20 13:49:43 +0100

    init

--- mytoolbox/my_files.py
+++ mytoolbox/my_files.py
@@ -8,7 +8,7 @@ def read_file(f_name: str) -> str:
     read text file
     """
     if not _is_abs_path(f_name):
-        f_name = get_project_dir_path() + "\\" + f_name
+        f_name = get_external_path() + "\\" + f_name
     with open(f_name, mode='r', encoding='utf-8', newline='') as file:
         return file.read()
 
@@ -18,16 +18,16 @@ def write_file(f_name: str, text: str):
     write text file
     """
     if not _is_abs_path(f_name):
-        f_name = get_project_dir_path() + "\\" + f_name
+        f_name = get_external_path() + "\\" + f_name
     with open(f_name, mode='w', encoding='utf-8', newline='') as file:
         print(text, file=file)
 
 
 def copy_file(source_path: str, destination_path: str):
     if not _is_abs_path(source_path):
-        source_path = get_project_dir_path() + "\\" + source_path
+        source_path = get_external_path() + "\\" + source_path
     if not _is_abs_path(destination_path):
-        destination_path = get_project_dir_path() + "\\" + destination_path
+        destination_path = get_external_path() + "\\" + destination_path
     # destination folders have to exist as first!!!
     from shutil import copyfile
     copyfile(source_path, destination_path)
@@ -35,9 +35,9 @@ def copy_file(source_path: str, destination_path: str):
 
 def copy_dir(source_dir: str, destination_dir: str, rewrite: bool = True):
     if not _is_abs_path(source_dir):
-        source_dir = get_project_dir_path() + "\\" + source_dir
+        source_dir = get_external_path() + "\\" + source_dir
     if not _is_abs_path(destination_dir):
-        destination_dir = get_project_dir_path() + "\\" + destination_dir
+        destination_dir = get_external_path() + "\\" + destination_dir
 
     from shutil import copytree, rmtree
     if rewrite:
@@ -51,7 +51,7 @@ def exist_file(file_name: str) -> bool:
     function return bool of existing file
     """
     if not _is_abs_path(file_name):
-        file_name = get_project_dir_path() + "\\" + file_name
+        file_name = get_external_path() + "\\" + file_name
     return os.path.isfile(file_name)
 
 
@@ -60,7 +60,7 @@ def exist_dir(directory_name: str) -> bool:
     function return bool of existing directory
     """
     if not _is_abs_path(directory_name):
-        directory_name = get_project_dir_path() + "\\" + directory_name
+        directory_name = get_external_path() + "\\" + directory_name
     return os.path.isdir(directory_name)
 
 
@@ -69,7 +69,7 @@ def get_abs_path(file_name: str) -> str:
     function return absolute path from relative file name
     """
     if not _is_abs_path(file_name):
-        file_name = get_project_dir_path() + "\\" + file_name
+        file_name = get_external_path() + "\\" + file_name
     return os.path.abspath(file_name)
 
 
@@ -94,7 +94,7 @@ def get_dir(file_path: str) -> str:
     function return absolute current directory path
     """
     if not _is_abs_path(file_path):
-        file_path = get_project_dir_path() + "\\" + file_path
+        file_path = get_external_path() + "\\" + file_path
     return os.path.dirname(file_path)
 
 
@@ -116,7 +116,7 @@ def make_dir(directory_name: str) -> bool:
     function create directory if it doesn't exist
     """
     if not _is_abs_path(directory_name):
-        directory_name = get_project_dir_path() + "\\" + directory_name
+        directory_name = get_external_path() + "\\" + directory_name
     if not exist_dir(directory_name):
         os.mkdir(directory_name)
         return True
@@ -125,7 +125,7 @@ def make_dir(directory_name: str) -> bool:
 
 def write_json(json_file_name: str, data: object):
     if not _is_abs_path(json_file_name):
-        json_file_name = get_project_dir_path() + "\\" + json_file_name
+        json_file_name = get_external_path() + "\\" + json_file_name
     try:
         j_data = json.dumps(data, ensure_ascii=False, indent=2)
         write_file(json_file_name, j_data)
@@ -135,7 +135,7 @@ def write_json(json_file_name: str, data: object):
 
 def read_json(json_file_name: str) -> str:
     if not _is_abs_path(json_file_name):
-        json_file_name = get_project_dir_path() + "\\" + json_file_name
+        json_file_name = get_external_path() + "\\" + json_file_name
     try:
         j_data = json.loads(read_file(json_file_name))
         return j_data
@@ -148,7 +148,7 @@ def get_files_with_extension(extension: str) -> list:
         extension = "." + extension
 
     files = []
-    for file in os.listdir(get_project_dir_path()):
+    for file in os.listdir(get_external_path()):
         if file.endswith(extension):
             files.append(file)
 
@@ -157,7 +157,7 @@ def get_files_with_extension(extension: str) -> list:
 
 def get_drive_from_path(file_path: str) -> str:
     if not _is_abs_path(file_path):
-        file_path = get_project_dir_path() + "\\" + file_path
+        file_path = get_external_path() + "\\" + file_path
     drive_tail = os.path.splitdrive(file_path)
     return drive_tail[0]  # Like - C:
     # return drive_tail[1]  # Like - \User\Documents\file.txt
@@ -180,7 +180,6 @@ def get_external_path() -> str:
     In case of PyCharm return e. g. C:\\Users\\konepa1\\PycharmProjects\\gFIT_steps_generator
     get_internal_path + get_external_path will have the same result in case of PyCharm
     """
-    curr_dir = os.path.dirname(get_project_dir_path())
     if getattr(sys, 'frozen', False):
         # one-file execution file folder
         return os.getcwd()
